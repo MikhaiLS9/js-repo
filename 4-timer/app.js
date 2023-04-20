7bde18f658e1e6ec3586a8a715a750908c1aeaa3
MIkhail <famm93@mail.ru>
2023-04-20 23:30:10 +0300

    4-timer добавил изменения в app.js и комментарий в index.html

--- 4-timer/app.js
+++ 4-timer/app.js
@@ -1,28 +1,32 @@
-const newYearTime = "2023-12-31 00:00:00";
+const newYearTime = new Date()
+newYearTime.setFullYear(newYearTime.getFullYear() + 1, 0,1)
+newYearTime.setHours(0,0,0)
 
-const MONTH = document.querySelector(".month");
-const DAYS = document.querySelector(".days");
-const HOURS = document.querySelector(".hours");
-const MINUTES = document.querySelector(".minutes");
-const SECONDS = document.querySelector(".seconds");
+const timerElements = {
+  month: document.querySelector(".month"),
+  days: document.querySelector(".days"),
+  hours: document.querySelector(".hours"),
+  minutes: document.querySelector(".minutes"),
+  seconds: document.querySelector(".seconds"),
+};
 
 function timer(time) {
   const nowData = new Date();
   const newYearDate = new Date(time);
   const lastYear = newYearDate - nowData;
 
-  const millisecondDays = Math.floor(lastYear / 86400000);
+  const MILLISECONDS_IN_DAYS = Math.floor(lastYear / 86400000);
 
-  const monthLeft = newYearDate.getMonth() - nowData.getMonth();
-  const hoursLeft = Math.floor(lastYear / 1000 / 60 / 60) % 24;
-  const minutesLeft = Math.floor(lastYear / 1000 / 60) % 60;
-  const secondsLeft = Math.floor(lastYear / 1000) % 60;
+  const MONTH_LEFT = newYearDate.getMonth() - nowData.getMonth() +11;
+  const MILLISECONDS_IN_HOURS = Math.floor(lastYear / 1000 / 60 / 60) % 24;
+  const MILLISECONDS_IN_MINUTES = Math.floor(lastYear / 1000 / 60) % 60;
+  const MILLISECONDS_IN_SECONDS = Math.floor(lastYear / 1000) % 60;
 
-  MONTH.innerHTML = String(monthLeft).padStart(2, "0");
-  DAYS.innerHTML = String(millisecondDays).padStart(2, "0");
-  HOURS.innerHTML = String(hoursLeft).padStart(2, "0");
-  MINUTES.innerHTML = String(minutesLeft).padStart(2, "0");
-  SECONDS.innerHTML = String(secondsLeft).padStart(2, "0");
+  timerElements.month.innerHTML = String(MONTH_LEFT).padStart(2, "0");
+  timerElements.days.innerHTML = String(MILLISECONDS_IN_DAYS).padStart(2, "0");
+  timerElements.hours.innerHTML = String(MILLISECONDS_IN_HOURS).padStart(2, "0");
+  timerElements.minutes.innerHTML = String(MILLISECONDS_IN_MINUTES).padStart(2, "0");
+  timerElements.seconds.innerHTML = String(MILLISECONDS_IN_SECONDS).padStart(2, "0");
 }
 timer(newYearTime);
 
