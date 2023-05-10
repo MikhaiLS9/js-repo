cc371f764a56b62bc51e72c2a200936a6d7dda04
MikhaiLS9 <famm93@mail.ru>
2023-05-10 20:24:26 +0300

    promise

--- 12-promise-race/app.js
+++ 12-promise-race/app.js
@@ -1,7 +1,9 @@
-async function race(...allPromise) {
-  const resolve_1 = await new Promise((resolve, reject) =>
-    [...allPromise].forEach((item) => item.then(resolve, reject))
+function race (allPromise) {
+  return new Promise((resolve, reject) =>
+    allPromise.forEach((item) => item.then(resolve, reject))
   );
-  return console.log(resolve_1);
-}
-race();
+};
+
+Promise.race([]).then((result) =>
+  console.log(result)
+);
